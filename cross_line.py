d306c08a27ec2f5f664129f850bb6b933f647c79
Joseph Avila Alvarez <josdavilalvarez@gmail.com>
2023-08-03 22:51:46 -0500

    Add cross line

--- cross_line.py
+++ cross_line.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import datetime
 
 # Cargar los archivos de configuración y pesos pre-entrenados de YOLO
-net = cv2.dnn.readNet("yolov3.cfg", "yolov3.weights")
+net = cv2.dnn.readNet("yolov3-tiny.cfg", "yolov3-tiny.weights")
 
 # Obtener las capas de salida de YOLO (detecciones)
 layer_names = net.getLayerNames()
@@ -16,62 +17,81 @@ with open("coco.names", "r") as f:
 # Definir los colores de las cajas de detección
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-# Definir las coordenadas del cuadro de interés (ROI)
-x_roi, y_roi, width_roi, height_roi = 100, 100, 300, 300
+# Definir las coordenadas de la línea que el carro debe cruzar (un poco a la izquierda)
+line_x = None
+line_y = None
+
+# Definir el desplazamiento hacia la izquierda (en píxeles)
+left_offset = 120
 
 # Crear una ventana de visualización
 cv2.namedWindow("Object Detection")
 
 # Capturar video desde la cámara
-captura = cv2.VideoCapture(0)
+captura = cv2.VideoCapture("test3.mp4")
+
+# Track car position to check for crossing the line
+car_crossed_line = False
 
 while True:
     # Leer el siguiente fotograma del video
     ret, frame = captura.read()
 
-    # Dibujar el cuadro de interés (ROI)
-    cv2.rectangle(frame, (x_roi, y_roi), (x_roi + width_roi, y_roi + height_roi), (0, 255, 0), 2)
+    if not ret:
+        break
 
-    # Obtener la región de interés (ROI)
-    roi = frame[y_roi:y_roi + height_roi, x_roi:x_roi + width_roi]
+    # Obtener las dimensiones del fotograma
+    height, width, _ = frame.shape
 
-    # Redimensionar el ROI para el procesamiento más rápido
-    blob = cv2.dnn.blobFromImage(roi, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    # Dibujar la línea de cruce un poco a la izquierda
+    if line_x is None:
+        line_x = width // 2 - left_offset
+        line_y = 0
+    cv2.line(frame, (line_x, line_y), (line_x, height), (255, 0, 0), 2)
 
-    # Pasar el ROI por la red de YOLO para obtener las detecciones
+    # Pasar el fotograma por la red de YOLO para obtener las detecciones
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
 
     # Procesar las detecciones obtenidas
-    class_ids = []
-    confidences = []
-    boxes = []
     for out in outs:
         for detection in out:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
 
-            if confidence > 0.5:
-                center_x = int(detection[0] * width_roi)
-                center_y = int(detection[1] * height_roi)
-                w = int(detection[2] * width_roi)
-                h = int(detection[3] * height_roi)
+            if confidence > 0.75:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
 
-                x = int(center_x - w / 2) + x_roi
-                y = int(center_y - h / 2) + y_roi
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
 
-                # Filtrar las detecciones que están completamente dentro del cuadro
-                if x > x_roi and y > y_roi and x + w < x_roi + width_roi and y + h < y_roi + height_roi:
-                    # Agregar la detección a las listas
-                    class_ids.append(class_id)
-                    confidences.append(float(confidence))
-                    boxes.append([x, y, w, h])
+                # Filtrar las detecciones que están completamente dentro del fotograma
+                if x >= 0 and y >= 0 and x + w <= width and y + h <= height:
+                    # Check if the detected object is a car
+                    if classes[class_id] == 'car':
+                        # Check if the car is inside the line crossing ROI
+                        if x <= line_x and x + w >= line_x:
+                            # Draw bounding box and label for the detected car
+                            color = colors[class_id]
+                            label = f"{classes[class_id]}: {confidence:.2f}"
+                            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-                    # Dibujar la caja de detección y etiqueta correspondiente
-                    color = colors[class_id]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                    cv2.putText(frame, classes[class_id], (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                            # Check if the car crossed the line
+                            if not car_crossed_line:
+                                car_crossed_line = True
+                                # Save the frame at the moment the car crossed the line
+                                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                                save_filename = f"car_capture_{timestamp}.jpg"
+                                cv2.imwrite(save_filename, frame)
+
+    # Reset the car_crossed_line variable at the end of each frame processing
+    car_crossed_line = False
 
     # Mostrar el fotograma actual en la ventana
     cv2.imshow("Object Detection", frame)
